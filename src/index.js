3c21aa80674e44a45f928705181215c1964112fd
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2017-01-14 18:03:14 -0500

    Configure Redux store and first action/reducer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import 'babel-polyfill';
 
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import { render } from 'react-dom';
 
+import configureStore from './store/configureStore';
 import routes from './routes';
 
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 render(
-    <Router history={browserHistory} routes={routes} />,
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
     document.getElementById('app')
 );
