c555838cb1750982908ad360382a3192bc44159a
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2017-01-14 18:44:36 -0500

    Use constant for action type

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,3 +1,5 @@
+import { CREATE_COURSE } from './actionTypes';
+
 export function createCourse(course) {
-    return { type: 'CREATE_COURSE', payload: course };
+    return { type: CREATE_COURSE, payload: course };
 }
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,8 @@
+import { CREATE_COURSE } from '../actions/actionTypes';
+
 export default function courseReducer(state = [], { type, payload: course }) {
     switch(type) {
-    case 'CREATE_COURSE':
+    case CREATE_COURSE:
         return [...state, Object.assign({}, course)];
     default:
         return state;
